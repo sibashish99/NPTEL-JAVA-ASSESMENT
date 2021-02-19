2b7e79d9c072ee7b3cdefd329ee3ee34ce6c3143
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-02-19 19:17:57 +0530

    Add files via upload

---
+++ Week five/three.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+  
+  public class Question5_3 {
+  public static void main(String[] args) { 
+      int a, b;
+      Scanner input = new Scanner(System.in);
+      import java.util.Scanner;
+  
+      public class Question5_3 {
+      public static void main(String[] args) { 
+          int a, b;
+          Scanner input = new Scanner(System.in);
+      }
+    }    
